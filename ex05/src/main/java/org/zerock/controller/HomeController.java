3c89fc718d0fa3f2b3a8cde278f8789534a26829
simkyubong <91923519+simkyubong@users.noreply.github.com>
2021-11-03 14:15:57 +0900

    2021_11_03 [수] 2번째 업로드

--- ex05/src/main/java/org/zerock/controller/HomeController.java
+++ ex05/src/main/java/org/zerock/controller/HomeController.java
@@ -16,10 +16,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
+	// 메인페이지 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
 		return "index2";
 	}
 	
+	@RequestMapping(value = "/index3", method = RequestMethod.GET)
+	public String index3() {
+		return "index3";
+		}
+	
+	@RequestMapping(value = "/index4", method = RequestMethod.GET)
+	public String index4() {
+		return "index4";
+		}
+	
+	@RequestMapping(value = "/index5", method = RequestMethod.GET)
+	public String index5() {
+		return "index5";
+		}
+	
+	@RequestMapping(value = "/header", method = RequestMethod.GET)
+	public String header() {
+		return "header";
+		}
+	
+	@RequestMapping(value = "/footer", method = RequestMethod.GET)
+	public String footer() {
+		return "footer";
+		}
+	
+
 	
 }
